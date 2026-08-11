f802e84e74e9a8d3912a70e843c0b93ec4706555
Rahim <abderhimedouh@gmail.com>
2026-08-11 23:26:15 +0200

    f

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { listPostsHandler, createPostHandler } from "./handlers/postHandler.js";
+import { listPostsHandler, createPostHandler } from "./handlers/postHandler.ts";
 
 const app = express();
 
